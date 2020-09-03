1a350f0537aa721a1829b71bb4a5be9d416da9d7
Jamal Ali <jamal@lctinteriorsolutions.co.uk>
2020-09-03 15:07:25 +0100

    styling for featured article section

--- dist/assets/theme.js
+++ dist/assets/theme.js
@@ -19964,18 +19964,6 @@ function init() {
     $(document).ready(function () {
 
         $('.slick-slider').slick();
-
-        $('.featured-products__products').slick({
-            centerMode: true,
-            centerPadding: '24px',
-            arrows: false,
-            infinite: false,
-            mobileFirst: true,
-            responsive: [{
-                breakpoint: 767,
-                settings: "unslick"
-            }]
-        });
     });
 }
 
