ddb419a1f55eff768eef250085b808e8c1ab8cb9
Jamal Ali <jamal@lctinteriorsolutions.co.uk>
2020-09-14 12:45:13 +0100

    styling updated to homepage sections, header is now fixed, scrolling disabled when left-hand menu is open

--- dist/assets/theme.js
+++ dist/assets/theme.js
@@ -20021,17 +20021,41 @@ var Marquee = exports.Marquee = function () {
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+var scrollY = void 0;
+
 function init() {
 
     var toggles = document.getElementsByClassName('mobile-menu-toggle');
-    var header = document.querySelector('.site-header');
+    var body = document.querySelector('body');
 
     for (var i = 0; i < toggles.length; i++) {
         toggles[i].addEventListener('click', function (event) {
             event.preventDefault();
-            header.classList.toggle('mobile-menu-open');
+            body.classList.toggle('mobile-menu-open');
+
+            if (body.classList.contains('mobile-menu-open')) {
+                disableScroll(body);
+            } else {
+                enableScroll(body);
+            }
         });
     }
+}
+
+function disableScroll(body) {
+
+    scrollY = window.scrollY;
+
+    // There are also some styles added to <body> in mobile-menu.js
+    body.style.position = 'fixed';
+    body.style.top = '-' + scrollY + 'px';
+}
+
+function enableScroll(body) {
+    body.style.position = '';
+    body.style.top = '';
+
+    window.scrollTo(0, scrollY);
 }
 
 exports.init = init;
--- src/resources/js/modules/mobile-menu.js
+++ src/resources/js/modules/mobile-menu.js
@@ -1,14 +1,38 @@
+let scrollY;
+
 function init() {
 
     let toggles = document.getElementsByClassName('mobile-menu-toggle');
-    let header = document.querySelector('.site-header');
+    let body = document.querySelector('body');
 
     for (var i=0; i<toggles.length; i++) {
         toggles[i].addEventListener('click', event => {
             event.preventDefault();
-            header.classList.toggle('mobile-menu-open');
+            body.classList.toggle('mobile-menu-open');
+
+            if (body.classList.contains('mobile-menu-open')) {
+                disableScroll(body);
+            } else {
+                enableScroll(body);
+            }
         });
     }
+}
+
+function disableScroll(body) {
+
+    scrollY = window.scrollY;
+
+    // There are also some styles added to <body> in mobile-menu.js
+    body.style.position = 'fixed';
+    body.style.top = `-${scrollY}px`;
+}
+
+function enableScroll(body) {
+    body.style.position = '';
+    body.style.top = '';
+
+    window.scrollTo(0, scrollY);
 }
 
 export { init };
